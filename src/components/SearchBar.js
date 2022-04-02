e42f42dd0b2280b2b7311f40338e90cb211ca3f9
simon3073 <simonbertoli@gmail.com>
2022-04-02 13:54:59 +1100

    Search functionality added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const SearchBar = (props) => {
+	let textInput = React.createRef();
+
+	function handleClick() {
+		props.buttonClick(textInput.current.value);
+	}
+
+	const handleKeyPress = (e) => {
+		if (e.key === 'Enter') {
+			props.buttonClick(textInput.current.value);
+		}
+	};
+
 	return (
 		<header className="header">
 			<h2 className="title">Search The News</h2>
-			<input type="text" id="filter" placeholder="What do you need to know??" />
-			<button>Give me the news!</button>
+			<input ref={textInput} type="text" id="search" placeholder="What do you need to know??" onKeyUp={handleKeyPress} />
+			<button onClick={handleClick}>Search for news</button>
 		</header>
 	);
 };
